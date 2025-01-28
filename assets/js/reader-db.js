863c3c08cbb27c44628a9a44da612f65ae0fd9be
AeolusDev <aeolusdeveloper@gmail.com>
2025-01-28 14:36:29 +0530

    Reader works now

--- assets/js/reader-db.js
+++ assets/js/reader-db.js
@@ -1,14 +1,28 @@
 let basedbUrl = "https://altscans-api.netlify.app";
-let series =  axios.get(`${basedbUrl}/api/admin/getSeries`, {
-	mangaId: "31868",
-	seriesName: "hclw",
-	chapterNo: "69"
-  }
-).then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(`error: `, error);
-  });
 
-console.log(series);
+document.addEventListener('DOMContentLoaded', function() {
+  let series = axios.get(`${basedbUrl}/api/admin/getSeries/31868/hclw/73`, {})
+    .then(function (response) {
+      let imageContainer = document.getElementById('chapter-content');
+      if (!imageContainer) {
+        // If the container doesn't exist, create it
+        imageContainer = document.createElement('div');
+        imageContainer.id = 'chapter-content';
+        const readerContent = document.querySelector('.reader-content');
+        if (readerContent) {
+          readerContent.appendChild(imageContainer);
+        } else {
+          document.body.appendChild(imageContainer);
+        }
+      }
+      const imagesHTML = response.data.resources.map((imageUrl, index) => 
+        `<img src="${imageUrl}" alt="Chapter 73 Page ${index + 1}" class="chapter-image">`
+      ).join('');
+      imageContainer.innerHTML = imagesHTML;
+    })
+    .catch(function (error) {
+      console.log(`error: `, error);
+    });
+
+  console.log(`Series Log:`, series);
+});
