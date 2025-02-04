841a1c52a19a9a418e75599b90132131be503835
AeolusDev <aeolusdeveloper@gmail.com>
2025-02-05 04:02:19 +0530

    reader changes

--- assets/js/reader-db.js
+++ assets/js/reader-db.js
@@ -21,36 +21,35 @@ document.addEventListener('DOMContentLoaded', function() {
     if (header) {
       header.textContent = 'Error: Missing URL parameters';
     }
+    return;
   }
-  
-  let series = axios.get(`${basedbUrl}/api/admin/getSeries/${seriesId}/${seriesName}/${chapterNo}`, {})
+    
+  axios.get(`${basedbUrl}/api/admin/getSeries/${seriesId}/${seriesName}/${chapterNo}`, {})
     .then(function (response) {
-      console.log(response)
+      console.log(response);
       let header = document.getElementsByClassName('chapter-title');
       let chapterNoHeader = document.getElementById('current-chapter');
       let chapterNameHeader = document.getElementById('current-chapter-name');
       let chapterDropdown = document.getElementById('chapter-select');
-      if(!chapterNoHeader || !chapterNameHeader) {
+      if (!chapterNoHeader || !chapterNameHeader) {
         // If Chapter No. doesn't exist, spawn it in
         chapterNoHeader = document.createElement('div');
         chapterNoHeader.id = 'current-chapter';
-
+  
         // If Chapter Name doesn't exist, spawn it in
         chapterNameHeader = document.createElement('div');
         chapterNameHeader.id = 'current-chapter-name';
-
+        
         // Add them to the header if it exists
         if (header && header[0]) {
           header[0].appendChild(chapterNoHeader);
           header[0].appendChild(chapterNameHeader);
         }
       }
-
+        
       // Update the chapter information
       chapterNoHeader.textContent = `Chapter ${chapterNo}`;
       chapterNameHeader.textContent = formatTitle(response.data.seriesDetails.title);
-
-
       
       let imageContainer = document.querySelector('.chapter-content');
       if (!imageContainer) {
@@ -64,8 +63,22 @@ document.addEventListener('DOMContentLoaded', function() {
           document.body.appendChild(imageContainer);
         }
       }
-      const imagesHTML = response.data.resources.map((imageUrl, index) => 
-        `<img src="${imageUrl}" alt="Chapter 73 Page ${index + 1}" class="chapter-image">`
+      
+      // Sort the images based on the page number extracted from the URL
+      const sortedImages = response.data.resources.sort((a, b) => {
+        const matchA = a.match(/_(\d+)\./);
+        const matchB = b.match(/_(\d+)\./);
+        if (!matchA || !matchB) {
+          console.log(`Failed to match URL pattern: ${!matchA ? a : b}`);
+          return 0;
+        }
+        const pageA = parseInt(matchA[1], 10);
+        const pageB = parseInt(matchB[1], 10);
+        return pageA - pageB;
+      });
+      
+      const imagesHTML = sortedImages.map((imageUrl, index) => 
+        `<img src="${imageUrl}" alt="Chapter ${chapterNo} Page ${index + 1}" class="chapter-image">`
       ).join('');
       imageContainer.innerHTML = imagesHTML;
     })
@@ -74,26 +87,24 @@ document.addEventListener('DOMContentLoaded', function() {
       let errorRes = error.response.request.response;
       
       if (errorRes) {
-        
         let header = document.querySelector('.chapter-title');
         if (header) {
           header.textContent = `Error: An Error Has Occured. Error: ${errorRes}`;
         }
       }
     });
-
-  console.log(`Series Log:`, series);
+        
+  console.log(`Series Log:`, seriesId);
   
   var disqus_config = function () {
-      this.page.url = window.location.href;  // Use current page URL
-      this.page.identifier = `${seriesName}-chapter-${chapterNo}`; // Create unique identifier based on series and chapter
+    this.page.url = window.location.href;  // Use current page URL
+    this.page.identifier = `${seriesName}-chapter-${chapterNo}`; // Create unique identifier based on series and chapter
   };
-
+  
   (function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://altscan.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
+    var d = document, s = d.createElement('script');
+    s.src = 'https://altscan.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
   })();
 });
-
