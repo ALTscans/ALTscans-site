543bdc52c984323d5caf0f41e3eea8c9af4b8a0c
AeolusDev <aeolusdeveloper@gmail.com>
2025-02-15 20:03:59 +0530

    Added 404

--- assets/js/details.js
+++ assets/js/details.js
@@ -4,19 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const seriesThumbnail = document.querySelector('image-container');
     let title = document.querySelector('.title');
     
-    let nick, seriesId;
+    // Get current URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const nick = urlParams.get('series');
+    const seriesId = urlParams.get('id');
+    console.log(nick, seriesId)
     
-    // Check if we're using the old or new URL format
-    if (window.location.search) {
-      // Old format with query parameters
-      const urlParams = new URLSearchParams(window.location.search);
-      nick = urlParams.get('series');
-      seriesId = urlParams.get('id');
-    } else {
-      // New format with path segments
-      const pathSegments = window.location.pathname.split('/');
-      nick = pathSegments[2];
-      seriesId = pathSegments[3];
+    // Update URL to clean format without page reload
+    if (nick && seriesId) {
+      const newUrl = `/series/?nick=${nick}&id=${seriesId}`;
+      history.pushState({}, '', newUrl);
     }
     
     function formatTitle(title) {
@@ -35,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     try {
-      const response = await axios.get(`https://altscans-api/api/admin/getSeriesDetails/${seriesId}/${nick}`)
+      const response = await axios.get(`http://localhost:8888/api/admin/getSeriesDetails/${seriesId}/${nick}`)
         .then(response => response.data.seriesDetails);
       
       console.log(response);
@@ -49,4 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   handleDetails();
+  
+  // Handle browser back/forward buttons
+  window.addEventListener('popstate', handleDetails);
 });
