4665b56ad54752738847995bd47c28744c8c35ea
AeolusDev <aeolusdeveloper@gmail.com>
2025-05-28 00:45:25 +0530

    refresh auth token

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 //Final Commit For Month - March 2025
 const base_url = `https://api.alternativescans.icu`;
-const frontoken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiQyF5YjlWY0hjQm5HcWpCRVlQdzhqTnNhQG5RI3V3IiwiaWF0IjoxNzQwODEyOTE1fQ.wImh8Y-s3jZtdEIyTvl9eUEh2VgG_NcjoqX-nlW1Zso`;
+const frontoken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiNlIxVVNhdHEhdE5oZGgkV05OaHAiLCJpYXQiOjE3NDgzNzMyODl9.qtzXR_F-xqq0p_gEr3FPu5I8-oaUQ2kkcVej4EMZ6qI`;
 const discordUrl = `https://discord.com/oauth2/authorize?client_id=1326916685655052369&response_type=code&redirect_uri=https%3A%2F%2Falternativescans.icu%2Flogin&scope=identify+email`;
 //For dev
 //const discordUrl = `https://discord.com/oauth2/authorize?client_id=1326916685655052369&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A6556%2Flogin&scope=identify+email`
